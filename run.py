4574eb2f71dff2f5734a977495c70cb19475f2c2
impshum <impshum@sausage.local>
2018-10-23 10:39:32 +0100

    up

---
+++ run.py
@@ -0,0 +1,56 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
+from halo import Halo
+import time
+
+
+likestool_user = 'XXXX'
+likestool_pass = 'XXXX'
+max_work = 60
+
+
+def likestool_login(browser):
+    browser.visit('https://likestool.com')
+    sleep(1)
+    browser.fill('LoginForm[username]', likestool_user)
+    browser.fill('LoginForm[password]', likestool_pass)
+    browser.find_by_xpath('//*[@id="login_form"]/input[2]').click()
+    sleep(5)
+
+
+def likestool_work(browser, page):
+    url = 'https://likestool.com/campaign/{}'.format(page)
+    browser.visit(url)
+    for x in range(max_work):
+        coins = browser.find_by_css('#coins').text
+        msg = '{}: {} points / {}'.format(x, coins, page)
+        spinner.text = msg
+        go = False
+        while not go:
+            try:
+                browser.find_by_css('.campaign_button.bg_red').click()
+                sleep(45)
+                go = True
+            except Exception:
+                browser.reload()
+        sleep(randint(7, 15))
+
+
+def slumber():
+    t = time.localtime()
+    t = time.mktime(t[:3] + (0, 0, 0) + t[6:])
+    t = t + 24 * 3600 - time.time()
+    time.sleep(t)
+
+
+spinner = Halo(text='Booting up...', spinner='dots')
+spinner.start()
+
+with Browser(headless=True, incognito=True, profile_preferences={'media.volume_scale': '0.0', 'media.autoplay.enabled': False, 'permissions.default.image': 2}) as browser:
+    likestool_login(browser)
+    while 1:
+        likestool_work(browser, 'DAILYMOTION_VIEWS')
+        likestool_work(browser, 'YOUTUBE_VIEWS')
+        slumber()
