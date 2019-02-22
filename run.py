55816f403ee749fff761f4afcb577151690e36a2
impshum <impshum@sausage.local>
2019-02-22 19:33:38 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
+from selenium.webdriver import ChromeOptions
 from splinter import Browser
 from bs4 import BeautifulSoup
+from time import sleep
 from random import randint
 from halo import Halo
-import time
-
 
 likestool_user = 'XXXX'
 likestool_pass = 'XXXX'
@@ -11,12 +11,12 @@ max_work = 60
 
 
 def likestool_login(browser):
-    browser.visit('https://likestool.com')
-    time.sleep(1)
+    browser.visit('https://likestool.com/site/login')
+    sleep(1)
     browser.fill('LoginForm[username]', likestool_user)
     browser.fill('LoginForm[password]', likestool_pass)
-    browser.find_by_xpath('//*[@id="login_form"]/input[2]').click()
-    time.sleep(5)
+    browser.find_by_xpath('//*[@id="login_form"]/div[4]/input').click()
+    sleep(5)
 
 
 def likestool_work(browser, page):
@@ -30,26 +30,28 @@ def likestool_work(browser, page):
         while not go:
             try:
                 browser.find_by_css('.campaign_button.bg_red').click()
-                time.sleep(45)
+                if page == 'DAILYMOTION_VIEWS':
+                    sleep(12)
+                    browser.windows.current = browser.windows[1]
+                    browser.execute_script("document.getElementById('dmp_Video').pause();")
+                    browser.windows.current = browser.windows[0]
+                    sleep(33)
+                else:
+                    sleep(45)
                 go = True
             except Exception:
                 browser.reload()
-        time.sleep(randint(7, 15))
-
-
-def slumber():
-    t = time.localtime()
-    t = time.mktime(t[:3] + (0, 0, 0) + t[6:])
-    t = t + 24 * 3600 - time.time()
-    time.sleep(t)
+        sleep(randint(7, 15))
 
 
 spinner = Halo(text='Booting up...', spinner='dots')
 spinner.start()
 
-with Browser(headless=True, incognito=True, profile_preferences={'media.volume_scale': '0.0', 'media.autoplay.enabled': False, 'permissions.default.image': 2}) as browser:
+chrome_options = ChromeOptions()
+chrome_options.add_argument('--autoplay-policy=user-gesture-required')
+
+with Browser('chrome', headless=True, incognito=True, options=chrome_options) as browser:
     likestool_login(browser)
-    while 1:
-        likestool_work(browser, 'DAILYMOTION_VIEWS')
-        likestool_work(browser, 'YOUTUBE_VIEWS')
-        slumber()
+    likestool_work(browser, 'YOUTUBE_VIEWS')
+    likestool_work(browser, 'DAILYMOTION_VIEWS')
+    likestool_work(browser, 'SOUNDCLOUD_LISTEN')
